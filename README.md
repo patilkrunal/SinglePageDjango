8200bf095ea9037950d3b9862701a7048e6736a5
patilkrunal <patilkrunal11.kp@gmail.com>
2021-03-20 19:19:10 +0530

    added few security configurations

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A "Bare minimum django single page application."
 ### Running
 
 ```python
-python app.py runserver 8000
+DEBUG=True SECRET_KEY=SOME-SECRET-KEY python app.py runserver 8000
 ```
 
 Open http://localhost:8000/ in browser
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from django.conf import settings
@@ -6,9 +7,10 @@ from django.http import HttpResponse
 
 
 settings.configure(
-  DEBUG=True,
-  SECRET_KEY="a-bad-secret",
-  ROOT_URLCONF=__name__,
+  DEBUG = os.environ.get("DEBUG", ""),
+  ALLOWED_HOST = ["*"],
+  SECRET_KEY = os.environ.get("SECRET_KEY", "a-bad-secret"),
+  ROOT_URLCONF = __name__,
 )
 
 
